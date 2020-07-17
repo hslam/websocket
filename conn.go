5d3bf03ca1c27f025af12bcc979ebbab06f65820
hslam <791874158@qq.com>
2020-07-18 00:52:02 +0800

    fix Read bug

--- conn.go
+++ conn.go
@@ -28,7 +28,7 @@ func (c *Conn) Read(b []byte) (n int, err error) {
 	if len(c.connBuffer) > 0 {
 		if len(b) >= len(c.connBuffer) {
 			copy(b, c.connBuffer)
-			c.connBuffer = c.connBuffer[len(b):]
+			c.connBuffer = c.connBuffer[len(c.connBuffer):]
 			return len(c.connBuffer), nil
 		}
 		copy(b, c.connBuffer[:len(b)])
