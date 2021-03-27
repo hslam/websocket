2d246186c6f39b77d528344967f29f7e40c88ffc
hslam <791874158@qq.com>
2021-03-27 23:43:52 +0800

    add more frame tests

--- frame_test.go
+++ frame_test.go
@@ -23,6 +23,43 @@ func TestFrame(t *testing.T) {
 		if !reflect.DeepEqual(f.PayloadData, f2.PayloadData) {
 			t.Error()
 		}
+		for i := 0; i < len(data); i++ {
+			var f3 = frame{}
+			n, _ := f3.Unmarshal(data[:i])
+			if n != 0 {
+				t.Error(n, len(data))
+			}
+		}
+	}
+	{
+		buf := make([]byte, 64*1024)
+		f := frame{FIN: 1, Opcode: BinaryFrame, Mask: 1, MaskingKey: []byte{1, 2, 3, 4}, PayloadData: make([]byte, 64)}
+		cp := frame{FIN: 1, Opcode: BinaryFrame, Mask: 1, MaskingKey: []byte{1, 2, 3, 4}, PayloadData: make([]byte, 64)}
+		data, _ := cp.Marshal(buf)
+		var f2 = frame{}
+		f2.Unmarshal(data)
+		if f.FIN != f2.FIN {
+			t.Error()
+		}
+		if f.Opcode != f2.Opcode {
+			t.Error()
+		}
+		if f.Mask != f2.Mask {
+			t.Error()
+		}
+		if !reflect.DeepEqual(f.MaskingKey, f2.MaskingKey) {
+			t.Error()
+		}
+		if !reflect.DeepEqual(f.PayloadData, f2.PayloadData) {
+			t.Error()
+		}
+		for i := 0; i < len(data); i++ {
+			var f3 = frame{}
+			n, _ := f3.Unmarshal(data[:i])
+			if n != 0 {
+				t.Error(n, len(data))
+			}
+		}
 	}
 	{
 		buf := make([]byte, 64*1024)
@@ -39,6 +76,13 @@ func TestFrame(t *testing.T) {
 		if !reflect.DeepEqual(f.PayloadData, f2.PayloadData) {
 			t.Error()
 		}
+		for i := 0; i < len(data); i++ {
+			var f3 = frame{}
+			n, _ := f3.Unmarshal(data[:i])
+			if n != 0 {
+				t.Error(n, len(data))
+			}
+		}
 	}
 	{
 		buf := make([]byte, 128*1024)
@@ -54,6 +98,13 @@ func TestFrame(t *testing.T) {
 		}
 		if !reflect.DeepEqual(f.PayloadData, f2.PayloadData) {
 			t.Error()
+		}
+		for i := 0; i < len(data); i++ {
+			var f3 = frame{}
+			n, _ := f3.Unmarshal(data[:i])
+			if n != 0 {
+				t.Error(n, len(data))
+			}
 		}
 	}
 	{
@@ -77,6 +128,13 @@ func TestFrame(t *testing.T) {
 		}
 		if !reflect.DeepEqual(f.PayloadData, f2.PayloadData) {
 			t.Error()
+		}
+		for i := 0; i < len(data); i++ {
+			var f3 = frame{}
+			n, _ := f3.Unmarshal(data[:i])
+			if n != 0 {
+				t.Error(n, len(data))
+			}
 		}
 	}
 }
