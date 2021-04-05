ee134c462625087ab0852e7d69fbb94d3c00361a
hslam <791874158@qq.com>
2021-04-06 00:30:12 +0800

    add Upgrade TLS test

--- websocket_test.go
+++ websocket_test.go
@@ -123,3 +123,58 @@ func TestUpgrade(t *testing.T) {
 	l.Close()
 	wg.Wait()
 }
+
+func TestUpgradeTLS(t *testing.T) {
+	network := "tcp"
+	addr := ":8080"
+	Serve := func(conn *Conn) {
+		for {
+			msg, err := conn.ReadMessage(nil)
+			if err != nil {
+				break
+			}
+			conn.WriteMessage(msg)
+		}
+		conn.Close()
+	}
+	l, _ := net.Listen(network, addr)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				break
+			}
+			ws, err := Upgrade(conn, testTLSConfig())
+			if err != nil {
+				break
+			}
+			if ws != nil {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					Serve(ws)
+				}()
+			}
+		}
+	}()
+	conn, err := Dial(network, addr, "/", testSkipVerifyTLSConfig())
+	if err != nil {
+		t.Error(err)
+	}
+	msg := "Hello World"
+	if err := conn.WriteMessage([]byte(msg)); err != nil {
+		t.Error(err)
+	}
+	data, err := conn.ReadMessage(nil)
+	if err != nil {
+		t.Error(err)
+	} else if string(data) != msg {
+		t.Error(string(data))
+	}
+	conn.Close()
+	l.Close()
+	wg.Wait()
+}
