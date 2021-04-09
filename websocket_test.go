8cef6437e1aa730bd8128f6e52650664d7e9bacc
hslam <791874158@qq.com>
2021-04-10 00:04:07 +0800

    add Upgrade Header test

--- websocket_test.go
+++ websocket_test.go
@@ -285,6 +285,64 @@ func TestUpgradeHTTP(t *testing.T) {
 			t.Error()
 		}
 	}
+	{
+		clientHandshake := func(c *Conn) error {
+			c.accept = accept(c.key)
+			reqHeader := "GET " + c.path + " HTTP/1.1\r\n"
+			reqHeader += "Host: " + c.address + "\r\n"
+			reqHeader += "Origin: *\r\n"
+			reqHeader += "Sec-WebSocket-Version: 13\r\n"
+			reqHeader += "Sec-WebSocket-Key: " + c.key + "\r\n\r\n"
+			_, err := c.conn.Write([]byte(reqHeader))
+			if err != nil {
+				return err
+			}
+			// Require successful HTTP response
+			// before switching to websocket protocol.
+			resp, err := http.ReadResponse(bufio.NewReader(c.conn), &http.Request{Method: "GET"})
+			if err == nil {
+				accept := resp.Header.Get("Sec-WebSocket-Accept")
+				if resp.Status == status && accept == c.accept {
+					return nil
+				}
+				err = errors.New("unexpected HTTP response: " + resp.Status)
+			}
+			return err
+		}
+		fakeDial := func(network, address, path string, config *tls.Config) (*Conn, error) {
+			var err error
+			netConn, err := net.Dial(network, address)
+			if err != nil {
+				return nil, err
+			}
+			if config != nil {
+				config.ServerName = address
+				tlsConn := tls.Client(netConn, config)
+				if err = tlsConn.Handshake(); err != nil {
+					tlsConn.Close()
+					return nil, err
+				}
+				netConn = tlsConn
+			}
+			conn := client(netConn, false, address, path)
+			err = clientHandshake(conn)
+			if err != nil {
+				conn.Close()
+				return nil, &net.OpError{
+					Op:   "dial-http",
+					Net:  network + " " + address,
+					Addr: nil,
+					Err:  err,
+				}
+			}
+			return conn, nil
+		}
+
+		_, err := fakeDial(network, addr, "/", nil)
+		if err == nil {
+			t.Error()
+		}
+	}
 	l.Close()
 	wg.Wait()
 }
